71f8a08cee58eb2175f84ed40337dba146541dd3
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-12-05 11:09:48 -0300

    Fluent API

--- Dados/ApplicationDbContext.cs
+++ Dados/ApplicationDbContext.cs
@@ -9,10 +9,19 @@ namespace Dados
     {
     }
 
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      //https://www.entityframeworktutorial.net/efcore/fluent-api-in-entity-framework-core.aspx
+      //configurações dos modelos, ao inves de mexer direto na tabela..altera aqui
+      //pode ser feito direto na classe tbm, mas o contra é que suja a classe
+      modelBuilder.Entity<Produto>().ToTable("Produto");
+      modelBuilder.Entity<Produto>().Property(p => p.Nome).HasMaxLength(50);
+      //modelBuilder.Entity<Produto>().HasKey(p => p.Nome); //caso não tenha ID, pode setar qume quiser pra PK
     }
 
     //mapeamento de classes para o Banco de dados
